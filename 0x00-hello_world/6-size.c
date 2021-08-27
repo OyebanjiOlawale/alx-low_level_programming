47745b0a53c00f7da3abeab0e725e1f87b80af74
oyebanjiolawale <oyebanji_olawale@yahoo.com>
2021-08-27 15:52:23 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,21 +9,20 @@
  */
 
  int main(void)
- { 
+{ 
+char c;
+int i;
+long li;
+long long lli;
+float f;
 
-	 char c;
-	 int i;
-	 long li;
-	 long long lli;
-	 float f;
 
-
-	printf("Size of a char: %Id byte(s)\n", sizeof(c));
-	printf("Size of a int: %Id byte(s)\n", sizeof(i));
-	printf("Size of a long: %Id byte(s)\n", sizeof(li));
-	printf("Size of a long: %Id byte(s)\n", sizeof(lli));
-	printf("Size of a float: %Id byte(s)\n", sizeof(f));
+printf("Size of a char: %Id byte(s)\n", sizeof(c));
+printf("Size of a int: %Id byte(s)\n", sizeof(i));
+printf("Size of a long: %Id byte(s)\n", sizeof(li));
+printf("Size of a long: %Id byte(s)\n", sizeof(lli));
+printf("Size of a float: %Id byte(s)\n", sizeof(f));
 	
-	return (0);
+return (0);
  }
 
