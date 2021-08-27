670a613745638fa769560282ca438b686c3030df
oyebanjiolawale <oyebanji_olawale@yahoo.com>
2021-08-27 13:31:32 -0700

    printf_5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - print the string in the put function
  *
@@ -6,8 +7,9 @@
  * this program prints "programming is like building a multilingual puzzle
  * Return: 0
  */
+
 int main(void)
 {
-rintf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
