74f0c0d70fb49dcb853641aa5639f99101a4cc7b
oyebanjiolawale <oyebanji_olawale@yahoo.com>
2021-08-27 13:29:38 -0700

    printf_5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - print the string in the put function
  *
@@ -9,6 +8,6 @@
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+rintf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
